7fc49306c227f63185e16406ef5d684e27c7fe71
HJin <guswlsdl7771@naver.com>
2025-04-30 11:03:41 +0900

    회원가입 구현

--- src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
@@ -21,8 +21,8 @@ public class ApplicationListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         //todo#12 application 시작시 테스트 계정인 admin,user 등록합니다. 만약 존재하면 등록하지 않습니다.
         DbConnectionThreadLocal.initialize();
-        User admin = new User("admin","관리자","12345","001111", User.Auth.ROLE_ADMIN,1_000_000, LocalDateTime.now(),null);
-        User user = new User("user","사용자","12345","001111", User.Auth.ROLE_USER,1_000_000, LocalDateTime.now(),null);
+        User admin = new User("admin","관리자","12345","20001111", User.Auth.ROLE_ADMIN,1_000_000, LocalDateTime.now(),null);
+        User user = new User("user","사용자","12345","20001111", User.Auth.ROLE_USER,1_000_000, LocalDateTime.now(),null);
 
         if(userService.getUser(admin.getUserId())==null){
             userService.saveUser(admin);
---
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/SignupPostController.java
@@ -0,0 +1,41 @@
+package com.nhnacademy.shoppingmall.controller.auth;
+
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
+import com.nhnacademy.shoppingmall.user.service.UserService;
+import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Slf4j
+@RequestMapping(method = RequestMapping.Method.POST,value = "/signupAction.do")
+public class SignupPostController implements BaseController {
+    private final UserService userService = new UserServiceImpl(new UserRepositoryImpl());
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("user_id");
+        String userName = req.getParameter("user_name");
+        String userPassword = req.getParameter("user_password");
+        String userBirth = req.getParameter("user_birth");
+        User.Auth userAuth = User.Auth.valueOf(req.getParameter("user_auth"));
+        int points = 1_000_000;
+        User user = new User(userId,userName,userPassword,userBirth,userAuth,points,LocalDateTime.now(),null);
+
+        log.debug("user: {} 생성완료", userId);
+
+        if(Objects.isNull(user)) {
+            throw new RuntimeException("user 생성 실패");
+        }
+
+        userService.saveUser(user);
+        log.debug("{} 생성완료",user.getUserId());
+
+        return "shop/main/index";
+    }
+}
